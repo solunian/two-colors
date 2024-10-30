6b6c873b5b09452eafb3fced7c28591da1b22f4a
Daniel Cui <solunian@gmail.com>
2024-10-29 20:36:34 -0700

    feat: scaffold love tetris

---
+++ love-ver/lib/board.lua
@@ -0,0 +1,68 @@
+local love = require("love")
+local table = require("table")
+local io = require("io")
+
+
+local b = {}
+
+-- constant values --
+
+b.ss = 20 -- square size for grid
+b.w = 10 -- width
+b.h = 20 -- height
+b.sh = 3 -- spawn height, playfield must have 3 extra spawn rows
+b.vals = { EMPTY = 0, FILLED = 1}
+
+
+-- playfield is upside down!
+-- (1,1) is bottom left, (20,20) is top right, rows 21-23 are spawn rows
+b.pf = {}
+
+-- reset and init playfield
+b.reinit_playfield = function ()
+  for row=1,b.h+b.sh do
+    local curr = {}
+    for col=1,b.w do
+      table.insert(curr, b.vals.EMPTY);
+    end
+    table.insert(b.pf, curr);
+  end
+end
+
+b.print_playfield = function (w, h)
+  for i=h,1,-1 do
+    for j=1,w do
+      io.write(b.pf[i][j] .. " ")
+    end
+    io.write("\n")
+  end
+end
+
+
+
+b.draw = function ()
+    -- draw current board with all the squares
+    for row=b.h+b.sh,1,-1 do
+      for col=1,b.w do
+        love.graphics.setColor(love.math.colorFromBytes(255, ((row * 10) % 255), ((col * 10) % 255)))
+        love.graphics.rectangle("fill", (col - 1) * b.ss, (row - 1) * b.ss, b.ss, b.ss);
+      end
+    end
+  -- draw grid lines
+  for row=b.sh,b.h+b.sh do -- extra one for bottom border
+    love.graphics.setLineWidth(1)
+    love.graphics.setColor(0, 0, 0, 1)
+    love.graphics.line(0, row * b.ss, b.w * b.ss, row * b.ss)
+  end
+
+  for col=0,b.w do -- extra one for bottom border
+    love.graphics.setLineWidth(1)
+    love.graphics.setColor(0, 0, 0, 1)
+    love.graphics.line(col * b.ss, 0, col * b.ss, (b.h + b.sh) * b.ss)
+  end
+
+
+end
+
+
+return b
---
+++ love-ver/main.lua
@@ -0,0 +1,16 @@
+local love = require("love")
+local board = require("lib.board")
+
+function love.load()
+  love.window.setTitle("two colors")
+
+  board.reinit_playfield()
+  board.print_playfield(board.w, board.h)
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+  board.draw()
+end
