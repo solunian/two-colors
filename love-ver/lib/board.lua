7d25f7b6af61f38bc77ac4bddbc2bf0dc23b59f2
Daniel Cui <solunian@gmail.com>
2024-10-31 19:22:49 -0700

    feat: kick/rot tables, board and mino scaffold

--- love-ver/lib/board.lua
+++ love-ver/lib/board.lua
@@ -2,7 +2,6 @@ local love = require("love")
 local table = require("table")
 local io = require("io")
 
-
 local b = {}
 
 -- constant values --
@@ -11,26 +10,26 @@ b.ss = 25 -- square size for grid
 b.w = 10 -- width
 b.h = 20 -- height
 b.sh = 3 -- spawn height, playfield must have 3 extra spawn rows
-b.type = { EMPTY = 1, FILLED = 2}
+b.ts = { EMPTY = 1, FILLED = 2} -- block types
 
 
 -- playfield is upside down!
--- (1,1) is bottom left, (20,20) is top right, rows 21-23 are spawn rows
+-- (3, 3) is top left, (23,23) is bottom right, rows 1-3 are spawn rows
 b.pf = {}
 
 -- reset and init playfield
 b.reinit_playfield = function ()
-  for _=1,b.h+b.sh do
+  for _=1,b.sh+b.h do
     local curr = {}
     for _=1,b.w do
-      table.insert(curr, b.type.EMPTY);
+      table.insert(curr, b.ts.EMPTY);
     end
     table.insert(b.pf, curr);
   end
 end
 
 b.print_playfield = function (w, h)
-  for i=h,1,-1 do
+  for i=1,h do
     for j=1,w do
       io.write(b.pf[i][j] .. " ")
     end
@@ -38,13 +37,11 @@ b.print_playfield = function (w, h)
   end
 end
 
-
-
 b.draw = function ()
     -- draw current board with all the squares
-    for row=b.h+b.sh,1,-1 do
+    for row=1,b.sh+b.h do
       for col=1,b.w do
-        if b.pf[row][col] == b.type.FILLED then
+        if b.pf[row][col] == b.ts.FILLED then
           love.graphics.setColor(love.math.colorFromBytes(255, ((row * 10) % 255), ((col * 10) % 255)))
           love.graphics.rectangle("fill", (col - 1) * b.ss, (row - 1) * b.ss, b.ss, b.ss);
         end
@@ -52,12 +49,11 @@ b.draw = function ()
     end
 
   -- draw grid lines
-  for row=b.sh,b.h+b.sh do -- extra one for bottom border
+  for row=b.sh,b.sh+b.h do -- extra one for bottom border
     love.graphics.setLineWidth(1)
     love.graphics.setColor(1, 1, 1, 1)
     love.graphics.line(0, row * b.ss, b.w * b.ss, row * b.ss)
   end
-
   for col=0,b.w do -- extra one for bottom border
     love.graphics.line(col * b.ss, 0, col * b.ss, (b.h + b.sh) * b.ss)
   end
--- love-ver/main.lua
+++ love-ver/main.lua
@@ -1,11 +1,15 @@
 local love = require("love")
+
 local board = require("lib.board")
+local minos = require("lib/minos")
 
 function love.load()
   love.window.setTitle("two colors")
 
   board.reinit_playfield()
   -- board.print_playfield(board.w, board.h)
+
+  minos.spawn(minos.ts.L)
 end
 
 function love.update(dt)
