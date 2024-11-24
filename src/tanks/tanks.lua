555cd408a2ecd587653be70e50315dfb6dbc4c41
Daniel Cui <solunian@gmail.com>
2024-11-23 17:43:04 -0800

    feat: tank aim thingy, kinda ugly

--- src/tanks/tanks.lua
+++ src/tanks/tanks.lua
@@ -38,6 +38,20 @@ end
 t.draw = function ()
   -- loop through tables to draw all of them
   for _,tank in pairs(tanks) do
+    -- no mouse?
+    love.mouse.setVisible(false)
+
+    -- tank aim line, aim crosshair
+    if tank.tank_type == tankclass.TANK_TYPES.P1 then
+      love.graphics.setColor(1, 1, 1, 0.5)
+      love.graphics.line(tank.x + tank.w / 2, tank.y + tank.h / 2, love.mouse.getX(), love.mouse.getY())
+
+      love.graphics.setColor(1, 1, 1, 1)
+      love.graphics.rectangle("fill", love.mouse.getX() - 10, love.mouse.getY() - 2, 20, 4)
+      love.graphics.rectangle("fill", love.mouse.getX() - 2, love.mouse.getY() - 10, 4, 20)
+    end
+
+    -- tank
     love.graphics.setColor(0, 1, 1, 1)
     love.graphics.rectangle("fill", tank.x, tank.y, tank.w, tank.h)
   end
